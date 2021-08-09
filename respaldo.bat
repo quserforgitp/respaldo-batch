de8aee5fd35797d7579d2f43d79fc0c2e15eb917
AutHor <adelaumnonegligas@gmail.com>
2021-08-09 16:26:09 -0500

    add copia estructura de arbol de directorios #5 con el propósito de recorrer los directorios y no tener que crearlos
    -> 8

--- respaldo.bat
+++ respaldo.bat
@@ -4,6 +4,9 @@ REM OBTENER RUTA ORIGEN Y RUTA DESTINO
 SET _rutaOrigen=C:\Users\IBM ThinkPad X230\Desktop\proyectosPersonales\respaldo\loc1
 SET _rutaDestino=C:\Users\IBM ThinkPad X230\Desktop\proyectosPersonales\respaldo\loc2
 
+REM COPIAR ESTRUCTURA DE ARBOL DEL ORIGEN AL DESTINO
+XCOPY "%_rutaOrigen%" "%_rutaDestino%" /T /E /Y
+
 REM COMPARAR CONTENIDOS
 
 REM COPIAR CONTENIDOS NO PRESENTES EN DESTINO
