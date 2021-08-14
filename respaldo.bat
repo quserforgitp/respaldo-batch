5cb10b901597a48ebbb0fe9ad2b3887b2640b9b6
AutHor <adelaumnonegligas@gmail.com>
2021-08-14 15:45:03 -0500

    add FOR recorre directorios y subdirectorios #7 recorre los directorios y los muestra, haciendo una pausa en cada uno. recorre a la ruta de origen.
    -> 16
    
    se habilita la expansion retardada para el for
    -> 15
    
    se eliminan los mensajes de debug para los directorios
    -> 8,9

--- respaldo.bat
+++ respaldo.bat
@@ -5,17 +5,21 @@ chcp 65001 > NUL
 REM OBTENER RUTA ORIGEN Y RUTA DESTINO
 SET _rutaOrigen=C:\Users\IBM ThinkPad X230\Desktop\proyectosPersonales\respaldo\loc1
 SET _rutaDestino=C:\Users\IBM ThinkPad X230\Desktop\proyectosPersonales\respaldo\loc2
-echo mostrando ruta actual %cd%
-pause
 
-REM COPIAR ESTRUCTURA DE ARBOL DEL ORIGEN AL DESTINO
+
+::COPIAR ESTRUCTURA DE ARBOL DEL ORIGEN AL DESTINO
 XCOPY "%_rutaOrigen%" "%_rutaDestino%" /T /E /Y
 
 REM COMPARAR CONTENIDOS
 CD "%_rutaOrigen%"
-
+SETLOCAL EnableDelayedExpansion
+FOR /D /R %%a IN (*) DO (
+	echo %%a
+	pause
+)
 
 REM COPIAR CONTENIDOS NO PRESENTES EN DESTINO
+
 
 REM EOF
 :EOF
