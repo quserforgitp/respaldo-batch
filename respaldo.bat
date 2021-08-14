258716c7abe443e4ebd86e047a189dcaa17bcb92
AutHor <adelaumnonegligas@gmail.com>
2021-08-14 15:35:39 -0500

    cambia a ruta origen estática #6

--- respaldo.bat
+++ respaldo.bat
@@ -1,13 +1,19 @@
 @ECHO OFF
 
+chcp 65001 > NUL
+
 REM OBTENER RUTA ORIGEN Y RUTA DESTINO
 SET _rutaOrigen=C:\Users\IBM ThinkPad X230\Desktop\proyectosPersonales\respaldo\loc1
 SET _rutaDestino=C:\Users\IBM ThinkPad X230\Desktop\proyectosPersonales\respaldo\loc2
+echo mostrando ruta actual %cd%
+pause
 
 REM COPIAR ESTRUCTURA DE ARBOL DEL ORIGEN AL DESTINO
 XCOPY "%_rutaOrigen%" "%_rutaDestino%" /T /E /Y
 
 REM COMPARAR CONTENIDOS
+CD "%_rutaOrigen%"
+
 
 REM COPIAR CONTENIDOS NO PRESENTES EN DESTINO
 
